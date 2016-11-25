962f1503ae754d4cd0b205c56c7c08f7869fb02d
Takuya Kuwahara <taakuu19@gmail.com>
2016-11-26 03:57:00 +0900

    impl YarnTask

--- src/main/java/me/taku_k/gradle/yarn/tasks/YarnTask.java
+++ src/main/java/me/taku_k/gradle/yarn/tasks/YarnTask.java
@@ -1,4 +1,30 @@
 package me.taku_k.gradle.yarn.tasks;
 
-public class YarnTask {
+import lombok.Getter;
+import lombok.Setter;
+import me.taku_k.gradle.yarn.runner.YarnExecRunner;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class YarnTask extends DefaultTask {
+    public static final String NAME = "";
+
+    private YarnExecRunner runner;
+
+    @Getter
+    @Setter
+    private List<String> args = new ArrayList<>();
+
+    public YarnTask() {
+        this.runner = new YarnExecRunner(this.getProject());
+        dependsOn( YarnSetupTask.NAME );
+    }
+
+    @TaskAction
+    public void exec() {
+        runner.exec(args);
+    }
 }
