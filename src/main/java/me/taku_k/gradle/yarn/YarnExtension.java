74beece975ff1dde2c469b2e27550e0b1191f03e
Takuya Kuwahara <taakuu19@gmail.com>
2016-11-26 03:51:07 +0900

    impl YarnExtension

--- src/main/java/me/taku_k/gradle/yarn/YarnExtension.java
+++ src/main/java/me/taku_k/gradle/yarn/YarnExtension.java
@@ -1,5 +1,27 @@
 package me.taku_k.gradle.yarn;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class YarnExtension {
     public static final String NAME = "yarn";
+
+    @Getter
+    @Setter
+    private String version = "";
+
+    @Getter
+    @Setter
+    private String workingDir = "yarn";
+
+    public String getBinPath() {
+        if ("".equals(version)) {
+            return "yarn";
+        }
+        return String.format(".gradle/%s/yarn-%s.js", workingDir, version);
+    }
+
+    public boolean isSpecifiedVersion() {
+        return !version.isEmpty();
+    }
 }
