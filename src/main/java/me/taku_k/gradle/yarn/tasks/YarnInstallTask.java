3e4d4a7d223766256da7e5c14b908e29139132b9
Takuya Kuwahara <taakuu19@gmail.com>
2016-11-26 03:55:35 +0900

    update YarnInstallTask

--- src/main/java/me/taku_k/gradle/yarn/tasks/YarnInstallTask.java
+++ src/main/java/me/taku_k/gradle/yarn/tasks/YarnInstallTask.java
@@ -1,12 +1,23 @@
 package me.taku_k.gradle.yarn.tasks;
 
-import org.gradle.api.internal.ConventionTask;
+import me.taku_k.gradle.yarn.runner.YarnExecRunner;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 
-public class YarnInstallTask extends ConventionTask {
+import java.util.Collections;
+import java.util.List;
+
+public class YarnInstallTask extends DefaultTask {
     public static final String NAME = "yarnInstall";
 
     public YarnInstallTask() {
         setGroup("Yarn");
         setDescription("Install node packages from yarn.lock or package.json.");
+    }
+
+    @TaskAction
+    public void install() {
+        List<String> args = Collections.singletonList("install");
+        new YarnExecRunner(getProject()).exec(args);
     }
 }
